9a0a7bb45cefed5aea8e34c4cad268836239c6ef
Rakibuzzaman Rahat <52344836+sleipnir029@users.noreply.github.com>
2023-11-25 16:19:43 +0600

    Update blog urls.py

--- blogsite/blog/urls.py
+++ blogsite/blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('change-password/', views.change_password, name='change_password')
 ]
