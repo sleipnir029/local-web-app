9b99077ce4b3209bc085ab255bb4862cc012e40e
Rakibuzzaman Rahat <52344836+sleipnir029@users.noreply.github.com>
2023-11-25 12:06:51 +0600

    Correcting errors

--- blogsite/blog/models.py
+++ blogsite/blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    created__at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
--- blogsite/blog/views.py
+++ blogsite/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import BlogPost
+from . import urls
 
-# Create your views here.
+def index(request):
+    posts = BlogPost.objects.all()  # Fetch all blog posts from the database
+    return render(request, 'index.html', {'posts': posts})
