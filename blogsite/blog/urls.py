eb528c47b62ba97bc7883d7676b5573c5471141d
Rakibuzzaman Rahat <52344836+sleipnir029@users.noreply.github.com>
2023-11-26 05:53:43 +0600

    unknown

--- blogsite/blog/urls.py
+++ blogsite/blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('change-password/', views.change_password, name='change_password'),
     #path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('post-create-update/', views.post_create_update, name='post_create_update'),
 ]
