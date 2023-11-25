2aadaf1e5b7f443fbfebb423d14034668cbebea3
Rakibuzzaman Rahat <52344836+sleipnir029@users.noreply.github.com>
2023-11-25 16:15:22 +0600

    Update views with login credentials

--- blogsite/blog/views.py
+++ blogsite/blog/views.py
@@ -1,7 +1,53 @@
-from django.shortcuts import render
+from math import log
+import re
+from urllib import request
+
+from django.shortcuts import render, redirect
+from django.urls import is_valid_path
 from .models import BlogPost
-from . import urls
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+
+from dotenv import load_dotenv
+import os
+
+def u_creds():
+    load_dotenv()
+    username = os.getenv('SECRET_USER')
+    password = os.getenv('SECRET_PASS')
+    
 
 def index(request):
     posts = BlogPost.objects.all()  # Fetch all blog posts from the database
     return render(request, 'index.html', {'posts': posts})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST[u_creds.username]
+        password = request.POST[u_creds.password]
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid credentials'})
+        
+    return render(request, 'login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return redirect('dashboard')
+    else:
+        form = PasswordChangeForm(user=request.user)
+    return render(request, 'change_password.html', {'form': form})
