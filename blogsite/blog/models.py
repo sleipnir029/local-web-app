93a53cbee6cc217f3e9f0c377f0e86d451bb0290
Rakibuzzaman Rahat <52344836+sleipnir029@users.noreply.github.com>
2023-11-25 11:39:34 +0600

    Update models and views

--- blogsite/blog/models.py
+++ blogsite/blog/models.py
@@ -1,7 +1,10 @@
+from pyexpat import model
 from django.db import models
-from django.shortcuts import render
-from .models import BlogPost
 
-def index(request):
-    posts = BlogPost.objects.all()
-    return render(request, 'index.html', {'posts': posts})
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created__at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
